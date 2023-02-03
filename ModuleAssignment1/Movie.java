be94b091785a3ad83aeb221f632ea2c972069d7b
antares67 <rasath87@students.rowan.edu>
2023-02-02 19:04:47 -0500

    Moved files from outside repo

---
+++ ModuleAssignment1/Movie.java
@@ -0,0 +1,43 @@
+public class Movie {
+
+    private String title;
+    private String releaseDate;
+    private int runtime;
+
+    public Movie(String title, String releaseDate, int runtime){
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.runtime = runtime;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+    public int getRuntime() {
+        return runtime;
+    }
+    public void setRuntime(int runtime) {
+         this.runtime = runtime;
+    }
+
+
+
+    @Override
+    public String toString(){
+        return ("Title: " + title + " " + "Release date: " + releaseDate + " " + "Runtime: " + runtime);
+    }
+    
+
+ 
+
+}
---
+++ ModuleAssignment1/MovieDriver.java
@@ -0,0 +1,53 @@
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class MovieDriver {
+	
+	private static HashMap <Integer, Movie> movies = new HashMap<Integer, Movie>();
+
+	public static void main(String[] args) {
+		populateMovies();
+		displayMovies();
+	}
+
+	/**
+	 * This method uses a Scanner and gets user input for three movies (title, length, release date
+	 * which it will then use to make a new Movie and add it into the HashMap movies.
+	 */
+	private static void populateMovies() {
+		System.out.println("Creating movies");
+		// TODO add your code here.
+	//	Scanner scan = new Scanner(System.in);
+		Movie movie1 = new Movie("Movie1", "Date1", 100);
+		Movie movie2 = new Movie("Movie2", "Date2", 150);
+		Movie movie3 = new Movie("Movie3", "Date3", 200);
+		// System.out.println("Movie Title");
+		movies.put(0, movie1);
+		// System.out.println("Movie Length");
+		movies.put(1, movie2);
+		// System.out.println("Movie release date");
+		movies.put(2, movie3);
+
+		//capitalCities.put("England", "London");
+
+
+		
+
+	}
+	
+	/**
+	 * This method loops through all the movies using the forEach method 
+	 * and displays each movie using its toString() method.
+	 */
+	private static void displayMovies() {
+		System.out.println("\nHere is the list of movies:");
+		// TODO add your code here.
+		for(int i = 0; i < movies.size(); i++){
+			System.out.println(movies.get(i));
+			
+		}
+		
+	}
+}
